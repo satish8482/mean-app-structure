dbe8f6b8e4bf804e90959c85351f0a7e923c5d5b
1243428 <a1243428@01HW849271.local>
2017-11-20 17:05:35 +0530

    mean_app structure example

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+   'port': process.env.PORT || 8000,
+  'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+   'secret': 'ilovescotchscotchyscotchscotch'
+ };
